7b31755a16ee5eeb56efac00692f2a8adf0cb5c9
daniellew712 <xiaoyuew66@gmail.com>
2025-04-01 14:28:22 -0700

    Frist three waves Item.py

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,16 @@
+import uuid
+
 class Item:
-    pass
+
+    def __init__(self, id = None, condition = 0):
+
+        if not id:
+            id = uuid.uuid4().int
+        self.id = id
+        self.condition =condition
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
